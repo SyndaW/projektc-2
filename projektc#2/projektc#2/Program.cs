fc180176fef8071bbef4efb0f6b863cc97d9330f
Vojtěch Synáč <vojtisek.synac@seznam.cz>
2024-09-25 13:49:33 +0200

    pridan projekt, zaciname

--- projektc#2/projektc#2/Program.cs
+++ projektc#2/projektc#2/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace projektc_2
+﻿using System;
+
+namespace KamenNuzkyPapir
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hrajeme Kámen, nůžky, papír!");
+            Console.WriteLine("Zadejte svůj tah (kámen, nůžky, papír): ");
+            string userChoice = Console.ReadLine();
+
+            Random random = new Random();
+            string[] choices = { "kámen", "nůžky", "papír" };
+            string computerChoice = choices[random.Next(choices.Length)];
+
+            Console.WriteLine(§"Počítač zvolil: {computerChoice}");
+
         }
     }
 }
